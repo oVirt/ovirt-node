9b880f5d6f40ec2ceb6767d338ae2298f1d4e3f3
Chris Lalancette <clalance@redhat.com>
2007-10-26 15:23:28 -0400

    Added a new script to create a "static" flash distribution.  This basically means the actual filesystem is laid down on the flash drive, the system boots from the flash drive, and changes on the filesystem are persistent.  This does have the danger in that changes you make here might not be saved, but it is much more convenient for development.  Note that currently you need a flash drive with minimum size of 1GB; you can change it by changing the ovirt.ks file to have a smaller part section.  However, the distribution really takes up at least 550MB, so there will not be a lot of wiggle room.

---
+++ ovirt-flash-static.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+if [ $# -eq 1 ]; then
+    ISO=
+elif [ $# -eq 2 ]; then
+    ISO=$2
+else
+    echo "Usage: ovirt-flash.sh <usbdevice> [iso-image]"
+    exit 1
+fi
+
+USBDEVICE=$1
+IMGTMP=/var/tmp/ovirt-$$
+SQUASHTMP=/var/tmp/ovirt-squash-$$
+USBTMP=/var/tmp/ovirt-usb-$$
+
+if [ ! -b "$USBDEVICE" ]; then
+    echo "USB device $USBDEVICE doesn't seem to exist"
+    exit 2
+fi
+
+if [ -z "$ISO" ]; then
+    # ISO image not provided on the command-line; build it
+    ./creator.py -c ovirt.ks -f ovirt
+    ISO=ovirt.iso
+fi
+echo $ISO
+
+# do setup
+mkdir -p $IMGTMP $SQUASHTMP $USBTMP
+mount -o loop $ISO $IMGTMP
+mount -o loop $IMGTMP/LiveOS/squashfs.img $SQUASHTMP
+
+# clear out the old partition table
+dd if=/dev/zero of=$USBDEVICE bs=4096 count=1
+echo -e 'n\np\n1\n\n\nt\n83\na\n1\nw\n' | /sbin/fdisk $USBDEVICE
+
+cat /usr/lib/syslinux/mbr.bin > $USBDEVICE
+dd if=$SQUASHTMP/LiveOS/ext3fs.img of=${USBDEVICE}1
+
+mount ${USBDEVICE}1 $USBTMP
+
+cp $IMGTMP/isolinux/* $USBTMP
+
+rm -f $USBTMP/isolinux.bin
+mv $USBTMP/isolinux.cfg $USBTMP/extlinux.conf
+
+sed -i -e 's/ *append.*/  append initrd=initrd.img root=LABEL=ovirt ro/' $USBTMP/extlinux.conf
+
+extlinux -i $USBTMP
+
+umount $USBTMP
+umount $SQUASHTMP
+umount $IMGTMP
+rm -rf $SQUASHTMP $IMGTMP $USBTMP
