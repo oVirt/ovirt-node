c59a2747dc1cf60cd51319075a882b164b3634e8
Scott Seago <sseago@redhat.com>
2007-11-27 15:40:31 -0500

    display host mem in GB

--- wui/src/app/models/host.rb
+++ wui/src/app/models/host.rb
@@ -1,5 +1,14 @@
+require 'util/ovirt'
+
 class Host < ActiveRecord::Base
   has_many :nics, :dependent => :destroy
   has_many :vms, :dependent => :nullify
   has_and_belongs_to_many :storage_volumes
+
+  def memory_in_gb
+    kb_to_gigs(memory)
+  end
+  def memory_in_gb=(mem)
+    self[:memory]=(gigs_to_kb(mem))
+  end
 end
