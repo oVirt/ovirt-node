378d78e85b6fe911e9b3a032da66bee0ad189cd0
Scott Seago <sseago@redhat.com>
2007-12-03 15:14:15 -0500

    added active task links for vm; added pending_task logic

--- wui/src/app/models/task.rb
+++ wui/src/app/models/task.rb
@@ -13,10 +13,53 @@ class Task < ActiveRecord::Base
   ACTION_SAVE_VM     = "save_vm"
   ACTION_RESTORE_VM  = "restore_vm"
 
+  # a hash of task actions which point to a hash which define valid state transitions
+  ACTIONS = { ACTION_CREATE_VM   => { :label => "Create VM",
+                                      :start => Vm::STATE_PENDING,
+                                      :running => Vm::STATE_CREATING,
+                                      :success => Vm::STATE_RUNNING,
+                                      :failure => Vm::STATE_CREATE_FAILED},
+              ACTION_START_VM    => { :label => "Start VM",
+                                      :start => Vm::STATE_STOPPED,
+                                      :running => Vm::STATE_STARTING,
+                                      :success => Vm::STATE_RUNNING,
+                                      :failure => Vm::STATE_STOPPED}, 
+              ACTION_SHUTDOWN_VM => { :label => "Shutdown VM",
+                                      :start => Vm::STATE_RUNNING,
+                                      :running => Vm::STATE_STOPPING,
+                                      :success => Vm::STATE_STOPPED,
+                                      :failure => Vm::STATE_RUNNING}, 
+              ACTION_SUSPEND_VM  => { :label => "Suspend VM",
+                                      :start => Vm::STATE_RUNNING,
+                                      :running => Vm::STATE_SUSPENDING,
+                                      :success => Vm::STATE_SUSPENDED,
+                                      :failure => Vm::STATE_RUNNING}, 
+              ACTION_RESUME_VM   => { :label => "Resume VM",
+                                      :start => Vm::STATE_SUSPENDED,
+                                      :running => Vm::STATE_RESUMING,
+                                      :success => Vm::STATE_RUNNING,
+                                      :failure => Vm::STATE_SUSPENDED},
+              ACTION_SAVE_VM     => { :label => "Save VM",
+                                      :start => Vm::STATE_RUNNING,
+                                      :running => Vm::STATE_SAVING,
+                                      :success => Vm::STATE_SAVED,
+                                      :failure => Vm::STATE_RUNNING},
+              ACTION_RESTORE_VM  => { :label => "Restore VM",
+                                      :start => Vm::STATE_SAVED,
+                                      :running => Vm::STATE_RESTORING,
+                                      :success => Vm::STATE_RUNNING,
+                                      :failure => Vm::STATE_SAVED} }
+
+
   STATE_QUEUED       = "queued"
   STATE_RUNNING      = "running"
   STATE_FINISHED     = "finished"
   STATE_PAUSED       = "paused"
   STATE_FAILED       = "failed"
+  STATE_CANCELED       = "canceled"
 
+  def cancel
+    self[:state] = STATE_CANCELED
+    save
+  end
 end
--- wui/src/app/models/vm.rb
+++ wui/src/app/models/vm.rb
@@ -1,7 +1,7 @@
 class Vm < ActiveRecord::Base
   belongs_to :user
   belongs_to :host
-  has_many :tasks, :dependent => :destroy
+  has_many :tasks, :dependent => :destroy, :order => "id DESC"
   has_and_belongs_to_many :storage_volumes
 
   NEEDS_RESTART_FIELDS = [:uuid, 
@@ -9,24 +9,77 @@ class Vm < ActiveRecord::Base
                           :memory_allocated,
                           :vnic_mac_addr]
 
-  STATE_PENDING    = "pending"
-  STATE_CREATING   = "creating"
-  STATE_RUNNING    = "running"
+  STATE_PENDING       = "pending"
+  STATE_CREATING      = "creating"
+  STATE_RUNNING       = "running"
 
-  STATE_STOPPING   = "stopping"
-  STATE_STOPPED    = "stopped"
-  STATE_STARTING   = "starting"
+  STATE_STOPPING      = "stopping"
+  STATE_STOPPED       = "stopped"
+  STATE_STARTING      = "starting"
 
-  STATE_SUSPENDING = "suspending"
-  STATE_SUSPENDED  = "suspended"
-  STATE_RESUMING   = "resuming"
+  STATE_SUSPENDING    = "suspending"
+  STATE_SUSPENDED     = "suspended"
+  STATE_RESUMING      = "resuming"
 
-  STATE_SAVING     = "saving"
-  STATE_SAVED      = "saved"
-  STATE_RESTORING  = "restoring"
+  STATE_SAVING        = "saving"
+  STATE_SAVED         = "saved"
+  STATE_RESTORING     = "restoring"
+  STATE_CREATE_FAILED = "create_failed"
+  STATE_INVALID       = "invalid"
 
-  STATE_DELETING   = "deleting"
-  STATE_MIGRATING  = "migrating"
-  STATE_UNKNOWN    = "unknown"
+  VALID_ACTIONS_PER_STATE = {  STATE_PENDING       => [Task::ACTION_CREATE_VM],
+                               STATE_RUNNING       => [Task::ACTION_SHUTDOWN_VM,
+                                                       Task::ACTION_SUSPEND_VM,
+                                                       Task::ACTION_SAVE_VM],
+                               STATE_STOPPED       => [Task::ACTION_START_VM],
+                               STATE_SUSPENDED     => [Task::ACTION_RESUME_VM],
+                               STATE_SAVED         => [Task::ACTION_RESTORE_VM],
+                               STATE_CREATE_FAILED => []}
 
+  EFFECTIVE_STATE = {  STATE_PENDING       => STATE_PENDING,
+                       STATE_CREATING      => STATE_RUNNING, 
+                       STATE_RUNNING       => STATE_RUNNING,
+                       STATE_STOPPING      => STATE_STOPPED,
+                       STATE_STOPPED       => STATE_STOPPED,
+                       STATE_STARTING      => STATE_RUNNING,
+                       STATE_SUSPENDING    => STATE_SUSPENDED,
+                       STATE_SUSPENDED     => STATE_SUSPENDED,
+                       STATE_RESUMING      => STATE_RUNNING,
+                       STATE_SAVING        => STATE_SAVED,
+                       STATE_SAVED         => STATE_SAVED,
+                       STATE_RESTORING     => STATE_RUNNING,
+                       STATE_CREATE_FAILED => STATE_CREATE_FAILED}
+  TASK_STATE_TRANSITIONS = []
+
+  def get_pending_state
+    pending_state = state
+    pending_state = EFFECTIVE_STATE[state] if pending_state
+    get_queued_tasks.each do |task|
+      return STATE_INVALID unless Task::ACTIONS[task.action][:start] == pending_state
+      pending_state = Task::ACTIONS[task.action][:success]
+    end
+    return pending_state
+  end
+
+  def get_queued_tasks(state=nil)
+    get_tasks(Task::STATE_QUEUED)
+  end
+
+  def get_tasks(state=nil)
+    conditions = "vm_id = '#{id}'"
+    conditions += " AND state = '#{Task::STATE_QUEUED}'" if state
+    Task.find(:all, 
+              :conditions => conditions,
+              :order => "id")
+  end    
+
+  def get_action_list
+    VALID_ACTIONS_PER_STATE[get_pending_state]
+  end
+
+  def get_action_and_label_list
+    get_action_list.collect do |action|
+      [Task::ACTIONS[action][:label], action]
+    end
+  end
 end
