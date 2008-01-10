14b6877599dfe362911cfdd18a3e9b999247c680
Scott Seago <sseago@denali.boston.redhat.com>
2008-01-10 15:51:22 -0500

    added quota-side permissions stuff

--- wui/src/app/controllers/quota_controller.rb
+++ wui/src/app/controllers/quota_controller.rb
@@ -20,83 +20,130 @@ class QuotaController < ApplicationController
                       ["Restore", Task::ACTION_RESTORE_VM]]
   end
 
+  def set_perms(perm_obj)
+    @user = get_login_user
+    @is_admin = perm_obj.is_admin(@user)
+    @can_monitor = perm_obj.can_monitor(@user)
+    @can_delegate = perm_obj.can_delegate(@user)
+  end
+
   def show
     @quota = Quota.find(params[:id])
-    @user = get_login_user
+    set_perms(@quota)
+    @is_hwgroup_admin = @quota.hardware_resource_group.is_admin(@user)
     @action_values = [["Suspend", Task::ACTION_SUSPEND_VM],
                       ["Resume", Task::ACTION_RESUME_VM],
                       ["Save", Task::ACTION_SAVE_VM],
                       ["Restore", Task::ACTION_RESTORE_VM]]
+    unless @can_monitor
+      flash[:notice] = 'You do not have permission to view this quota: redirecting to top level'
+      redirect_to :action => 'list'
+    end
                       
   end
 
   def new
     @quota = Quota.new( { :hardware_resource_group_id => params[:hardware_resource_group] } )
+    set_perms(@quota.hardware_resource_group)
+    unless @is_admin
+      flash[:notice] = 'You do not have permission to create a quota '
+      redirect_to :controller => 'pool', :action => 'show', :id => @quota.hardware_resource_group
+    end
   end
 
   def create
     @quota = Quota.new(params[:quota])
-    if @quota.save
-      flash[:notice] = 'Quota was successfully created.'
+    set_perms(@quota.hardware_resource_group)
+    unless @is_admin
+      flash[:notice] = 'You do not have permission to create a quota '
       redirect_to :controller => 'pool', :action => 'show', :id => @quota.hardware_resource_group
     else
-      render :action => 'new'
+      if @quota.save
+        flash[:notice] = 'Quota was successfully created.'
+        redirect_to :controller => 'pool', :action => 'show', :id => @quota.hardware_resource_group
+      else
+        render :action => 'new'
+      end
     end
   end
 
   def edit
     @quota = Quota.find(params[:id])
+    set_perms(@quota.hardware_resource_group)
+    unless @is_admin
+      flash[:notice] = 'You do not have permission to edit this quota '
+      redirect_to :action => 'show', :id => @quota
+    end
   end
 
   def update
     @quota = Quota.find(params[:id])
-    if @quota.update_attributes(params[:quota])
-      flash[:notice] = 'Quota was successfully updated.'
+    set_perms(@quota.hardware_resource_group)
+    unless @is_admin
+      flash[:notice] = 'You do not have permission to edit this quota '
       redirect_to :action => 'show', :id => @quota
     else
-      render :action => 'edit'
+      if @quota.update_attributes(params[:quota])
+        flash[:notice] = 'Quota was successfully updated.'
+        redirect_to :action => 'show', :id => @quota
+      else
+        render :action => 'edit'
+      end
     end
   end
 
   def destroy
     @quota = Quota.find(params[:id])
-    group_id = @quota.hardware_resource_group_id
-    @quota.destroy
-    redirect_to :controller => 'pool', :action => 'show', :id => group_id
+    set_perms(@quota.hardware_resource_group)
+    unless @is_admin
+      flash[:notice] = 'You do not have permission to delete this quota '
+      redirect_to :action => 'show', :id => @quota
+    else
+      group_id = @quota.hardware_resource_group_id
+      @quota.destroy
+      redirect_to :controller => 'pool', :action => 'show', :id => group_id
+    end
   end
 
   def vm_actions
-    params[:vm_actions].each do |name, param|
-      print "param: ", name, ", ", param, "\n"
-    end
-    if params[:vm_actions][:vms]
-      vms = params[:vm_actions][:vms]
-      if params[:vm_actions][Task::ACTION_START_VM]
-        flash[:notice] = "Starting Machines #{vms.join(',')}."
-      elsif params[:vm_actions][Task::ACTION_SHUTDOWN_VM]
-        flash[:notice] = "Stopping Machines #{vms.join(',')}."
-      elsif params[:vm_actions][:other_actions]
-        case params[:vm_actions][:other_actions]
-        when Task::ACTION_SHUTDOWN_VM then flash[:notice] = "Stopping Machines #{vms.join(',')}."
-        when Task::ACTION_START_VM then flash[:notice] = "Starting Machines #{vms.join(',')}."
-        when Task::ACTION_SUSPEND_VM then flash[:notice] = "Suspending Machines #{vms.join(',')}."
-        when Task::ACTION_RESUME_VM then flash[:notice] = "Resuming Machines #{vms.join(',')}."
-        when Task::ACTION_SAVE_VM then flash[:notice] = "Saving Machines #{vms.join(',')}."
-        when Task::ACTION_RESTORE_VM then flash[:notice] = "Restoring Machines #{vms.join(',')}."
-        when "destroy" then flash[:notice] = "Destroying Machines #{vms.join(',')}."
+    @quota = Quota.find(params[:quota_id])
+    set_perms(@quota)
+    unless @is_admin
+      flash[:notice] = 'You do not have permission to perform VM actions for this quota '
+      redirect_to :action => 'show', :id => @quota
+    else
+      params[:vm_actions].each do |name, param|
+        print "param: ", name, ", ", param, "\n"
+      end
+      if params[:vm_actions][:vms]
+        vms = params[:vm_actions][:vms]
+        if params[:vm_actions][Task::ACTION_START_VM]
+          flash[:notice] = "Starting Machines #{vms.join(',')}."
+        elsif params[:vm_actions][Task::ACTION_SHUTDOWN_VM]
+          flash[:notice] = "Stopping Machines #{vms.join(',')}."
+        elsif params[:vm_actions][:other_actions]
+          case params[:vm_actions][:other_actions]
+          when Task::ACTION_SHUTDOWN_VM then flash[:notice] = "Stopping Machines #{vms.join(',')}."
+          when Task::ACTION_START_VM then flash[:notice] = "Starting Machines #{vms.join(',')}."
+          when Task::ACTION_SUSPEND_VM then flash[:notice] = "Suspending Machines #{vms.join(',')}."
+          when Task::ACTION_RESUME_VM then flash[:notice] = "Resuming Machines #{vms.join(',')}."
+          when Task::ACTION_SAVE_VM then flash[:notice] = "Saving Machines #{vms.join(',')}."
+          when Task::ACTION_RESTORE_VM then flash[:notice] = "Restoring Machines #{vms.join(',')}."
+          when "destroy" then flash[:notice] = "Destroying Machines #{vms.join(',')}."
+          else
+            flash[:notice] = 'No Action Chosen.'
+          end
         else
           flash[:notice] = 'No Action Chosen.'
         end
       else
-        flash[:notice] = 'No Action Chosen.'
-      end
-    else
         flash[:notice] = 'No Virtual Machines Selected.'
-    end
-    if params[:vm_actions][:quota_id]
-    redirect_to :action => 'show', :id => params[:vm_actions][:quota_id]
-    else
-      redirect_to :action => 'list'
+      end
+      if params[:vm_actions][:quota_id]
+        redirect_to :action => 'show', :id => params[:vm_actions][:quota_id]
+      else
+        redirect_to :action => 'list'
+      end
     end
   end
 end
--- wui/src/app/controllers/vm_controller.rb
+++ wui/src/app/controllers/vm_controller.rb
@@ -1,20 +1,23 @@
 class VmController < ApplicationController
-  def index
-    list
-    render :action => 'list'
-  end
-
   # GETs should be safe (see http://www.w3.org/2001/tag/doc/whenToUseGet.html)
   verify :method => :post, :only => [ :destroy, :create, :update ],
          :redirect_to => { :action => :list }
 
-  def list
-    @vms = Vm.find(:all)
+  def set_perms(perm_obj)
+    @user = get_login_user
+    @is_admin = perm_obj.is_admin(@user)
+    @can_monitor = perm_obj.can_monitor(@user)
+    @can_delegate = perm_obj.can_delegate(@user)
   end
 
   def show
     @vm = Vm.find(params[:id])
     @actions = @vm.get_action_and_label_list
+    set_perms(@vm.quota)
+    unless @can_monitor
+      flash[:notice] = 'You do not have permission to view this vm: redirecting to top level'
+      redirect_to :controller => 'quota', :action => 'list'
+    end
   end
 
   def new
@@ -29,96 +32,135 @@ class VmController < ApplicationController
       :uuid => uuid
     }
     @vm = Vm.new( newargs )
+    set_perms(@vm.quota)
+    unless @is_admin
+      flash[:notice] = 'You do not have permission to create this vm'
+      redirect_to :controller => 'quota', :action => 'show', :id => @vm.quota_id
+    end
   end
 
   def create
     params[:vm][:state] = Vm::STATE_PENDING
     @vm = Vm.new(params[:vm])
-    user = get_login_user
-    if @vm.save
-      @task = Task.new({ :user    => user,
-                         :vm_id   => @vm.id,
-                         :action  => Task::ACTION_CREATE_VM,
-                         :state   => Task::STATE_QUEUED})
-      if @task.save
-        flash[:notice] = 'Vm was successfully created.'
-      else
-        flash[:notice] = 'Error in inserting task.'
-      end
-      redirect_to :controller => 'quota', :action => 'show', :id => @vm.quota
+    set_perms(@vm.quota)
+    unless @is_admin
+      flash[:notice] = 'You do not have permission to create this vm'
+      redirect_to :controller => 'quota', :action => 'show', :id => @vm.quota_id
     else
-      render :action => 'new'
+      if @vm.save
+        @task = Task.new({ :user    => @user,
+                           :vm_id   => @vm.id,
+                           :action  => Task::ACTION_CREATE_VM,
+                           :state   => Task::STATE_QUEUED})
+        if @task.save
+          flash[:notice] = 'Vm was successfully created.'
+        else
+          flash[:notice] = 'Error in inserting task.'
+        end
+        redirect_to :controller => 'quota', :action => 'show', :id => @vm.quota
+      else
+        render :action => 'new'
+      end
     end
   end
 
   def edit
     @vm = Vm.find(params[:id])
+    set_perms(@vm.quota)
+    unless @is_admin
+      flash[:notice] = 'You do not have permission to edit this vm'
+      redirect_to :action => 'show', :id => @vm
+    end
   end
 
   def update
     @vm = Vm.find(params[:id])
-    #needs restart if certain fields are changed (since those will only take effect the next startup)
-    needs_restart = false
-    unless @vm.get_pending_state == Vm::STATE_STOPPED
-      Vm::NEEDS_RESTART_FIELDS.each do |field|
-        unless @vm[field].to_s == params[:vm][field]
-          needs_restart = true
-          break
-        end
-      end
-      current_storage_ids = @vm.storage_volume_ids.sort
-      new_storage_ids = params[:vm][:storage_volume_ids]
-      new_storage_ids = [] unless new_storage_ids
-      new_storage_ids = new_storage_ids.sort.collect {|x| x.to_i }
-      needs_restart = true unless current_storage_ids == new_storage_ids
-    end
-    params[:vm][:needs_restart] = 1 if needs_restart
-    if @vm.update_attributes(params[:vm])
-      flash[:notice] = 'Vm was successfully updated.'
+    set_perms(@vm.quota)
+    unless @is_admin
+      flash[:notice] = 'You do not have permission to edit this vm'
       redirect_to :action => 'show', :id => @vm
     else
-      render :action => 'edit'
+      #needs restart if certain fields are changed (since those will only take effect the next startup)
+      needs_restart = false
+      unless @vm.get_pending_state == Vm::STATE_STOPPED
+        Vm::NEEDS_RESTART_FIELDS.each do |field|
+          unless @vm[field].to_s == params[:vm][field]
+            needs_restart = true
+            break
+          end
+        end
+        current_storage_ids = @vm.storage_volume_ids.sort
+        new_storage_ids = params[:vm][:storage_volume_ids]
+        new_storage_ids = [] unless new_storage_ids
+        new_storage_ids = new_storage_ids.sort.collect {|x| x.to_i }
+        needs_restart = true unless current_storage_ids == new_storage_ids
+      end
+      params[:vm][:needs_restart] = 1 if needs_restart
+      if @vm.update_attributes(params[:vm])
+        flash[:notice] = 'Vm was successfully updated.'
+        redirect_to :action => 'show', :id => @vm
+      else
+        render :action => 'edit'
+      end
     end
   end
 
   def destroy
     @vm = Vm.find(params[:id])
-    quota = @vm.quota_id
-    if @vm.state == Vm::STATE_STOPPED and @vm.get_pending_state == Vm::STATE_STOPPED
-      @vm.destroy
-      if quota
-        redirect_to :controller => 'quota', :action => 'show', :id => quota
-      else
-        redirect_to :controller => 'quota', :action => 'list'
-      end
+    set_perms(@vm.quota)
+    unless @is_admin
+      flash[:notice] = 'You do not have permission to delete this vm'
+      redirect_to :action => 'show', :id => @vm
     else
-      flash[:notice] = "Vm must be stopped to destroy it."
-      redirect_to :controller => 'vm', :action => 'show', :id => params[:id]
+      quota = @vm.quota_id
+      if @vm.state == Vm::STATE_STOPPED and @vm.get_pending_state == Vm::STATE_STOPPED
+        @vm.destroy
+        if quota
+          redirect_to :controller => 'quota', :action => 'show', :id => quota
+        else
+          redirect_to :controller => 'quota', :action => 'list'
+        end
+      else
+        flash[:notice] = "Vm must be stopped to destroy it."
+        redirect_to :controller => 'vm', :action => 'show', :id => params[:id]
+      end
     end
   end
 
   def vm_action
     @vm = Vm.find(params[:id])
-    if @vm.get_action_list.include?(params[:vm_action])
-      @task = Task.new({ :user    => get_login_user,
-                         :vm_id   => params[:id],
-                         :action  => params[:vm_action],
-                         :state   => Task::STATE_QUEUED})
-      if @task.save
-        flash[:notice] = "#{params[:vm_action]} was successfully queued."
-      else
-        flash[:notice] = "Error in inserting task for #{params[:vm_action]}."
-      end
+    set_perms(@vm.quota)
+    unless @is_admin
+      flash[:notice] = 'You do not have permission to schedule actions for this vm'
+      redirect_to :action => 'show', :id => @vm
     else
-      flash[:notice] = "#{params[:vm_action]} is an invalid action."
+      if @vm.get_action_list.include?(params[:vm_action])
+        @task = Task.new({ :user    => get_login_user,
+                           :vm_id   => params[:id],
+                           :action  => params[:vm_action],
+                           :state   => Task::STATE_QUEUED})
+        if @task.save
+          flash[:notice] = "#{params[:vm_action]} was successfully queued."
+        else
+          flash[:notice] = "Error in inserting task for #{params[:vm_action]}."
+        end
+      else
+        flash[:notice] = "#{params[:vm_action]} is an invalid action."
+      end
+      redirect_to :controller => 'vm', :action => 'show', :id => params[:id]
     end
-    redirect_to :controller => 'vm', :action => 'show', :id => params[:id]
   end
 
   def cancel_queued_tasks
     @vm = Vm.find(params[:id])
-    @vm.get_queued_tasks.each { |task| task.cancel}
-    flash[:notice] = "queued tasks canceled."
-    redirect_to :controller => 'vm', :action => 'show', :id => params[:id]
+    set_perms(@vm.quota)
+    unless @is_admin
+      flash[:notice] = 'You do not have permission to cancel actions for this vm'
+      redirect_to :action => 'show', :id => @vm
+    else
+      @vm.get_queued_tasks.each { |task| task.cancel}
+      flash[:notice] = "queued tasks canceled."
+      redirect_to :controller => 'vm', :action => 'show', :id => params[:id]
+    end
   end
 end
