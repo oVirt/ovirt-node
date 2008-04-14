e1d786c4e6ba2bd0a36c03395a90b6b0c290d280
Chris Lalancette <clalance@redhat.com>
2008-04-14 11:13:00 -0400

    Change up create-wui-appliance to be able to either create a production or a developer appliance.  This will just make life easier for developers in the future.  Currently I've only tested the developer side of things, but it still seems to do the right thing.  Next I have to work on and test the production side.

--- wui-appliance/create-wui-appliance.sh
+++ wui-appliance/create-wui-appliance.sh
@@ -12,7 +12,7 @@ IMGSIZE=6
 
 ISO=
 IMGDIR_DEFAULT=/var/lib/libvirt/images
-ARCH_DEFAULT=$(uname -i)
+ARCH_DEFAULT=$(uname -p)
 
 ARCH=$ARCH_DEFAULT
 IMGDIR=$IMGDIR_DEFAULT
@@ -51,28 +51,94 @@ case $ARCH in
     *) usage "invalid architecture: \`$ARCH'";;
 esac
 
-gen_dummy() {
-cat <<\EOF
+gen_bridge() {
+name=${1}bridge
+addr=$2
+cat << EOF
 <network>
-  <name>dummy</name>
-  <bridge name="dummybridge" stp="off" forwardDelay="0" />
-  <ip address="192.168.50.1" netmask="255.255.255.0"/>
+  <name>$name</name>
+  <bridge name="$name" stp="off" forwardDelay="0" />
+  <ip address="$addr" netmask="255.255.255.0"/>
 </network>
 EOF
 }
 
-# TODO when virFileReadAll is fixed for stdin
-#virsh net-define <(gen_dummy)
-TMPXML=$(mktemp) || exit 1
-gen_dummy > $TMPXML
-virsh net-define $TMPXML
-rm $TMPXML
-virsh net-start dummy
-virsh net-autostart dummy
+gen_fake_managed_node() {
+num=$1
+last_mac=$(( 54 + $num ))
 
-mkdir -p $IMGDIR
-virsh destroy $NAME > /dev/null 2>&1
-virsh undefine $NAME > /dev/null 2>&1
-virt-install -n $NAME -r $RAM -f "$IMGDIR/$IMGNAME" -s $IMGSIZE --vnc \
-             --accelerate -v -c "$ISO" --os-type=linux --arch=$ARCH \
-             -w network:default -w network:dummy
+cat <<EOF
+<domain type='kvm'>
+  <name>node$num</name>
+  <uuid>25ab2490-7c4c-099f-b647-${num}5ff8efa73f6</uuid>
+  <memory>524288</memory>
+  <currentMemory>524288</currentMemory>
+  <vcpu>1</vcpu>
+  <os>
+    <type>hvm</type>
+    <boot dev='network'/>
+  </os>
+  <features>
+    <acpi/>
+  </features>
+  <clock offset='utc'/>
+  <on_poweroff>destroy</on_poweroff>
+  <on_reboot>restart</on_reboot>
+  <on_crash>destroy</on_crash>
+  <devices>
+    <emulator>/usr/bin/qemu-kvm</emulator>
+    <interface type='network'>
+      <mac address='00:16:3e:12:34:$last_mac'/>
+      <source network='dummy'/>
+    </interface>
+    <input type='mouse' bus='ps2'/>
+    <graphics type='vnc' port='-1' listen='127.0.0.1'/>
+  </devices>
+</domain>
+EOF
+}
+
+if [ $ME = "create-wui-appliance.sh" ]; then
+    # TODO when virFileReadAll is fixed for stdin
+    #virsh net-define <(gen_dummy)
+    TMPXML=$(mktemp) || exit 1
+    gen_bridge "dummy" "192.168.50.1" > $TMPXML
+    virsh net-define $TMPXML
+    rm $TMPXML
+    virsh net-start dummy
+    virsh net-autostart dummy
+
+    # define the fake managed nodes we will use
+    for i in `seq 3 5` ; do
+	virsh undefine node$i >& /dev/null
+	TMPXML=$(mktemp)
+	gen_fake_managed_node $i > $TMPXML
+	virsh define $TMPXML
+	rm $TMPXML
+    done
+
+    mkdir -p $IMGDIR
+    virsh destroy $NAME > /dev/null 2>&1
+    virsh undefine $NAME > /dev/null 2>&1
+    virt-install -n $NAME -r $RAM -f "$IMGDIR/$IMGNAME" -s $IMGSIZE --vnc \
+        --accelerate -v -c "$ISO" --os-type=linux --arch=$ARCH \
+        -w network:default -w network:dummy
+elif [ $ME = "setup-prod.sh" ]; then
+    TMPXML=$(mktemp) || exit 1
+    gen_bridge "eth1" "192.168.25.1" > $TMPXML
+    virsh net-define $TMPXML
+    rm $TMPXML
+    virsh net-start eth1bridge
+    virsh net-autostart eth1bridge
+    
+    /usr/sbin/brctl addif eth1bridge eth1
+    
+    virsh destroy $NAME > /dev/null 2>&1
+    virsh undefine $NAME > /dev/null 2>&1
+    virt-install -n $NAME -r $RAM -f "$IMGDIR/$IMGNAME" -s $IMGSIZE --vnc \
+        --accelerate -v -c "$ISO" --os-type=linux --arch=$ARCH \
+        -w network:default -w network:eth1bridge
+else
+    usage "This script must be run as either create-wui-appliance.sh or setup-prod.sh"
+fi
+
