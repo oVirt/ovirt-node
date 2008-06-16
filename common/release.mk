da8f3761b2d343489b7b359538860424ddd6c3e0
Jim Meyering <meyering@redhat.com>
2008-06-16 20:43:35 +0200

    factor common variable definitions and rules out of Makefiles
    * common/release.mk: New file, factored out of...
    * ovirt-host-creator/Makefile: ...here, and...
    * wui/Makefile: ...here.
    Plus, add .PHONY: dependents.
    And rename "NAME" to "pkg_name".

---
+++ common/release.mk
@@ -0,0 +1,47 @@
+# Release/version-related Makefile variables and rules.
+# This Makefile snippet is included by both ../wui/Makefile and
+# ../ovirt-host-creator/Makefile.  It expects the including Makefile
+# to define the "pkg_name" variable, as well as a file named "version"
+# in the current directory.
+
+ARCH		:= $(shell uname -i)
+VERSION		:= $(shell awk '{ print $$1 }' version)
+RELEASE		:= $(shell awk '{ print $$2 }' version)
+NEWVERSION	= $$(awk 'BEGIN { printf "%.2f", $(VERSION) + .01 }')
+NEWRELEASE	= $$(($(RELEASE) + 1))
+X		= $$(awk '{ split($$2,r,"."); \
+                            printf("%d.%d\n", r[1], r[2]+1) }' version)
+git_head	= $$(git show-ref --hash=7 HEAD)
+GITRELEASE	= $(X).$$(date --utc +%Y%m%d%H%M)git$(git_head)
+DIST		= $$(rpm --eval '%{dist}')
+
+SPEC_FILE	= $(pkg_name).spec
+
+NV		= $(pkg_name)-$(VERSION)
+RPM_FLAGS	= \
+  --define "_topdir	%(pwd)/rpm-build" \
+  --define "_builddir	%{_topdir}" \
+  --define "_rpmdir	%{_topdir}" \
+  --define "_srcrpmdir	%{_topdir}" \
+  --define '_rpmfilename %%{NAME}-%%{VERSION}-%%{RELEASE}.%%{ARCH}.rpm' \
+  --define "_specdir	%{_topdir}" \
+  --define "_sourcedir	%{_topdir}"
+
+bumpgit:
+	echo "$(VERSION) $(GITRELEASE)" > version
+
+bumprelease:
+	echo "$(VERSION) $(NEWRELEASE)" > version
+
+bumpversion:
+	echo "$(NEWVERSION) 0" > version
+
+setversion:
+	echo "$(VERSION) $(RELEASE)" > version
+
+new-rpms: bumprelease rpms
+
+rpms: tar
+	rpmbuild $(RPM_FLAGS) -ba $(SPEC_FILE)
+
+.PHONY: rpms new-rpms setversion bumprelease bumpversion bumpgit
