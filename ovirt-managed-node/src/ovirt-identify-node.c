dbc8630d2aaee96a7241c192dd7160a007bac77e
Jim Meyering <meyering@redhat.com>
2008-06-30 16:16:42 +0200

    fix a bug in ovirt-identify-node.c's safewrite function
    * ovirt-identify-node.c (safewrite): Revert part of
    2d3498a5718931e4239aa223a4ad6ff7f2e08e91.  This function
    would malfunction upon any short write.

--- ovirt-managed-node/src/ovirt-identify-node.c
+++ ovirt-managed-node/src/ovirt-identify-node.c
@@ -283,7 +283,7 @@ ssize_t safewrite(int fd, const void *buf, size_t count)
 {
         size_t nwritten = 0;
         while (count > 0) {
-                ssize_t r = write(fd, buf+nwritten, count);
+                ssize_t r = write(fd, buf, count);
 
                 if (r < 0 && errno == EINTR)
                         continue;
