a029b38071f6b9736717cb05a911362c9d057d77
Perry Myers <pmyers@redhat.com>
2008-09-03 08:29:26 -0400

    Created ovirt-node repository and removed references to 'Managed'

--- misc-scripts/find_iscsi_devices.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-for sd in `ls -d /sys/block/sd*` ; do
-        vendor=`cat $sd/device/vendor | sed -e 's/[ \t]*$//'`
-        if [ "$vendor" = "IET" ]; then
-                echo "ISCSI"
-        fi
-done
