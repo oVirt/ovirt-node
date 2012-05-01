0b50f97841f924457ec50f33a2e3366e8c45f473
Joey Boggs <jboggs@redhat.com>
2012-05-01 18:12:39 -0400

    change snmp password after entering new one
    rhbz#795524
    
    Change-Id: Idff8f66fce3ec94d924668ace25b07a9f6946fcd
    Signed-off-by: Joey Boggs <jboggs@redhat.com>

--- scripts/snmp.py
+++ scripts/snmp.py
@@ -5,12 +5,20 @@ import os
 def enable_snmpd(password):
     conf = "/etc/snmp/snmpd.conf"
     system("service snmpd stop")
+    # get old password #
+    cmd="cat /etc/snmp/snmpd.conf |grep createUser|awk '{print $4}'"
+    oldpwd = subprocess.Popen(cmd, shell=True, stdout=PIPE, stderr=STDOUT)
+    oldpwd = oldpwd.stdout.read().strip()
     system("sed -c -ie '/^createUser root/d' %s" % conf)
     f = open(conf, "a")
     # create user account
     f.write("createUser root SHA %s AES\n" % password)
     f.close()
     system("service snmpd start")
+    # change existing password
+    if oldpwd > 0:
+        pwd_change_cmd = "snmpusm -v 3 -u root -n \"\" -l authNoPriv -a SHA -A %s localhost passwd %s %s -x AES" % (oldpwd, oldpwd, password)
+        system(pwd_change_cmd)
     ovirt_store_config(conf)
 
 def disable_snmpd():
