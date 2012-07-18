e864c209838197e846f19b2f9de765fecb259d05
Fabian Deutsch <fabiand@fedoraproject.org>
2012-07-18 16:22:15 +0200

    tests: Ignore vcs content on login
    Sometimes the vcs is not yet filled because the appropriate systemd
    service is not yet started or only started on tty activity, therefor we
    don't check for the login prompt but just enter the username as
    expected.
    
    Change-Id: Ia652e2be272475dfc4641f98f7fa5e1978d4d66b
    Signed-off-by: Fabian Deutsch <fabiand@fedoraproject.org>

--- tests/igor/tcs/uinput/login_as_admin.py
+++ tests/igor/tcs/uinput/login_as_admin.py
@@ -13,7 +13,8 @@ import common.input
 
 story = [
     # Enter Nothing, wait 0 seconds, expect "Please Login" on screen
-    (None,                0, "Please login"),
+#    (None,                0, "Please login"), # Ignore this for now.
+    ([" "],                 2, None), # Dummy to get a getty
 
     # Enter …, wait … seconds, expect … on screen
     (["admin\n"],           2, "Password:"),
