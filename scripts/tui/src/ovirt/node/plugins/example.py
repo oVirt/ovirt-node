cee069c12833505d2722f61774aa110c99482ec6
Fabian Deutsch <fabiand@fedoraproject.org>
2012-09-12 16:14:48 +0200

    Move to src subfolder
    Change-Id: Iba4b1ca40f12e9ac1b52cdf9e756dbb07f1da3b0
    Signed-off-by: Fabian Deutsch <fabiand@fedoraproject.org>

--- scripts/tui/src/ovirt/node/plugins/example.py
+++ scripts/tui/src/ovirt/node/plugins/example.py
@@ -16,3 +16,13 @@ class Plugin(ovirt.node.plugins.NodePlugin):
         widgets.append(ovirt.node.plugins.Label("Subsection"))
         widgets.append(ovirt.node.plugins.Entry("foo.bar", label=__file__))
         return widgets
+
+    def ui_on_change(self, model):
+        """Called when some widget was changed
+        """
+        LOGGER.debug("changed: " + str(model))
+
+    def ui_on_save(self, model):
+        """Called when data should be saved
+        """
+        LOGGER.debug("saved: " + str(model))
