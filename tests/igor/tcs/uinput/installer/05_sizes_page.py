5bdb49883e995ed39e60df9540fd5966aede6deb
Fabian Deutsch <fabiand@fedoraproject.org>
2012-10-17 11:36:39 +0200

    tests: Add sizes page to mi_extended
    Change-Id: Ifdf0f152028c750beab612c1d47fb8baafcdb17b
    Signed-off-by: Fabian Deutsch <fabiand@fedoraproject.org>

---
+++ tests/igor/tcs/uinput/installer/05_sizes_page.py
@@ -0,0 +1,30 @@
+#!/bin/env python
+# -*- coding: utf-8 -*
+# vim: set sw=4:
+
+import sys
+import os
+import logging
+import time
+
+sys.path.append(os.environ["IGOR_LIBDIR"])
+import common.common
+import common.input
+
+
+logger = logging.getLogger(__name__)
+
+title = "TUI Installer Sizes Page"
+
+story = [
+    # Expect sizes page
+    (None,                  4, "Please enter the sizes for"),
+]
+
+story += [
+    # Keep default values
+    (["\t\t\t\t\t\t\n"],            0, None),
+]
+
+if __name__ == "__main__":
+    common.input.Storyboard(title, story).run_and_exit()
