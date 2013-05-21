a9d5cae4092dabe641400addb6a75d03bcede867
Joey Boggs <jboggs@redhat.com>
2013-05-21 13:08:52 -0400

    ovirt-node-upgrade: fix logfile name
    Change-Id: I37ff3ecedc0f1e4d67e822a64686846fbac3b52e
    Signed-off-by: Joey Boggs <jboggs@redhat.com>

--- scripts/ovirt-node-upgrade.py
+++ scripts/ovirt-node-upgrade.py
@@ -50,7 +50,7 @@ def which(file):
 
 def initLogger():
     logger = logging.getLogger(LOG_PREFIX)
-    log_file = "/var/log/ovirt-upgrade.log"
+    log_file = "/var/log/ovirt-node-upgrade.log"
     formatter = logging.Formatter(fmt='%(asctime)s - %(levelname)-8s - '
                                   '%(module)s - %(message)s')
     conformatter = logging.Formatter('%(name)-12s: %(levelname)-8s'
