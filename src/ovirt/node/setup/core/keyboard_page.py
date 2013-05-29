f35039c4c8ad51ffe5dd60ba39d3102b67dea917
hadong <hadong0720@gmail.com>
2013-05-29 10:06:43 +0800

    keyboard page: Shown the default configured value
    Previously the keyboard page didn't show the default
    configured value when enter into this page.
    Now it works.
    
    Change-Id: I18b362fb93d1761138a4d64f01824ceb01677a9a
    Bug-Url: https://bugzilla.redhat.com/show_bug.cgi?id=966367
    Signed-off-by: hadong <hadong0720@gmail.com>

--- src/ovirt/node/setup/core/keyboard_page.py
+++ src/ovirt/node/setup/core/keyboard_page.py
@@ -52,7 +52,7 @@ class Plugin(plugins.NodePlugin):
         kbd = utils.system.Keyboard()
         ws = [ui.Header("header", "Keyboard Layout Selection"),
               ui.Table("keyboard.layout", "", "Available Keyboard Layouts",
-                       kbd.available_layouts()),
+                       kbd.available_layouts(), kbd.get_current()),
               ]
 
         page = ui.Page("page", ws)
