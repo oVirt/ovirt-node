4e5735673238b6f97d13dded76ba0200f0cfaeb7
Fabian Deutsch <fabiand@fedoraproject.org>
2013-07-16 16:36:48 +0200

    test: Add testcase to check binary dependencies
    The new testcase checks some binaries for missing library dependencies.
    
    Change-Id: I150c759c1e4f63f9f120b3f4d9403f9dcd8560b5
    Signed-off-by: Fabian Deutsch <fabiand@fedoraproject.org>

---
+++ tests/igor/tcs/check_bin_lib_dependencies.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+#
+# Check that all library requirements of
+# well known binaries are met
+#
+igor_highlight() { echo "= $@ ="; }
+igor_debug() { echo "[DEBUG] $@"; }
+COMMONLIB=${IGOR_LIBDIR}/common/common.sh
+[[ -e $COMMONLIB ]] && . $COMMONLIB
+
+FAILED=false
+
+BINARIES="/usr/bin/qemu-system-*"
+
+igor_highlight "Checking the following binaries for missing libraries: $BINARIES" "="
+echo ""
+
+for BINARY in $BINARIES;
+do
+  igor_highlight "Checking $BINARY"
+  
+  if ldd $BINARY | grep -q "not found";
+  then
+    igor_debug "Missing dependencies found:"
+    ldd $BINARY
+    FAILED=true
+  fi
+done
+
+if $FAILED;
+then
+  exit 1
+fi
+
+exit 0
