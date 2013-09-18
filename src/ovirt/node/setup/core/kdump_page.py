5d4214851de51e068947d3991f3547ea25c32690
Fabian Deutsch <fabiand@fedoraproject.org>
2013-09-18 10:29:03 +0200

    kdump: Remove unneeded (and removed) validation
    Previously the validation was called to ensure that a value read from a
    config file was valid. Due to changes the plugins.validate() method is
    not available anymore and the check was additionally not necessary.
    
    Change-Id: Ied731e09db97f4692a35ba1f32c98715d886461a
    Signed-off-by: Fabian Deutsch <fabiand@fedoraproject.org>

--- src/ovirt/node/setup/core/kdump_page.py
+++ src/ovirt/node/setup/core/kdump_page.py
@@ -127,7 +127,6 @@ class Plugin(plugins.NodePlugin):
             w = "kdump.%s_location" % changes["kdump.type"]
             if w in net_types and w in self.widgets:
                 self.widgets[w].enabled(True),
-                self.validate({w: ""})
 
     def on_merge(self, effective_changes):
         """Applies the changes to the plugins model, will do all required logic
