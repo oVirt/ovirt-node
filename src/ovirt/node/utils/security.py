oVirt/ovirt-node

commit eb0b20acd2594b067440f63e781f2fb8ee992cf2

Deprecate ovirtfunctions.rng_status()
Do something more pythonic, merge into the existing
ovirt.node.utils.security.Ssh class

Change-Id: I98730b91618a2f3cb65c7058c9fbe2ad50b80f11
Signed-off-by: Ryan Barry <rbarry@redhat.com>

src/ovirt/node/utils/security.py
@@ -23,6 +23,7 @@ from ovirt.node.utils import system
 from ovirt.node.utils.fs import File
 import PAM as _PAM  # @UnresolvedImport
 import cracklib
+from collections import namedtuple
 import hashlib
 import os.path
 import process
@@ -157,18 +158,15 @@ class Ssh(base.Base):
         Returns:
             The status of aes_ni
         """
-        import ovirtnode.ovirtfunctions as ofunc
-        rng, aes = ofunc.rng_status()
+        rng, aes = self.rng_status()
         if disable in [True, False]:
             self.__update_profile(rng, disable)
         else:
             self.logger.warning("Unknown value for AES NI: %s" % disable)
-        return ofunc.rng_status()[1]  # FIXME should rurn bool
-        # and does it return disable_aes_ni?
+        return self.rng_status().disable_aesni
 
     def strong_rng(self, num_bytes=None):
-        import ovirtnode.ovirtfunctions as ofunc
-        rng, aes = ofunc.rng_status()
+        rng, aes = self.rng_status()
         if (valid.Empty() | valid.Number(bounds=[0, None])).\
            validate(num_bytes):
             self.__update_profile(num_bytes, aes)
@@ -177,7 +175,23 @@ class Ssh(base.Base):
         else:
             self.logger.warning("Unknown value for RNG num bytes: " +
                                 "%s" % num_bytes)
-        return ofunc.rng_status()[0]
+        return self.rng_status().rng_bytes
+
+    def rng_status(self):
+        rng_bytes = None
+        disable_aes_ni = False
+
+        status = namedtuple("rngstatus", ["rng_bytes", "disable_aes_ni"])
+
+        f = File("/etc/profile")
+        if f.findall(r'SSH_USE_STRONG_RNG=\d+'):
+            rng_bytes = f.findall(r'SSH_USE_STRONG_RNG=\d+'
+                                  )[0].split('=')[1]
+        if f.findall(r'DISABLE_AES_NI='):
+            disable_aes_ni = True
+
+        rng_status = status(rng_bytes, disable_aes_ni)
+        return rng_status
 
     def restart(self):
         self.logger.debug("Restarting SSH")

src/ovirt/node/config/migrate.py
@@ -127,11 +127,12 @@ class ImportConfigs(base.Base):
 
     def translate_ssh(self):
         from ovirt.node.utils import parse_bool
+        from ovirt.node.utils.security import Ssh
 
         if self.__is_persisted("/etc/ssh/sshd_config"):
             pw_auth_enabled = ovirtfunctions.augtool_get(
                 "/files/etc/ssh/sshd_config/PasswordAuthentication")
-            rng_bytes, aes_disabled = ovirtfunctions.rng_status()
+            rng_bytes, aes_disabled = Ssh().rng_status()
 
             rng_bytes = None if rng_bytes == 0 else rng_bytes
             aes_disabled = aes_disabled == 1

src/ovirtnode/ovirtfunctions.py
@@ -1769,22 +1769,8 @@ def is_capslock_on():
     return "on" == output.strip()
 
 def rng_status():
-    bit_value = 0
-    disable_aes_ni = 0
-    try:
-        with open("/etc/profile") as f:
-            for line in f:
-                try:
-                    if "SSH_USE_STRONG_RNG" in line:
-                        export , kv = line.split()
-                        key, bit_value = kv.split("=")
-                    elif "OPENSSL_DISABLE_AES_NI=" in line:
-                        disable_aes_ni = 1
-                except:
-                    pass
-    except:
-        pass
-    return (bit_value, disable_aes_ni)
+    from ovirt.node.utils.security import Ssh
+    return Ssh().rng_status()
 
 def get_cmdline_args():
     with open("/proc/cmdline") as cmdline:

commit 237ff8cfc5c29ab51dd702b1d8787b102a8a069c

security.py: Fix typo in variable
The commit eb0b20ac introduced an error due a typo
in a variable name. This patch fix the typo and avoid
an error be showed to users when enabling/disabling SSH
via TUI.

Change-Id: I5c4f2be28eb26666a31cfa2259e5b36458ac8e58
Signed-off-by: Douglas Schilling Landgraf <dougsland@redhat.com>

src/ovirt/node/utils/security.py
@@ -162,7 +162,7 @@ class Ssh(base.Base):
             self.__update_profile(disable_aes=disable)
         else:
             self.logger.warning("Unknown value for AES NI: %s" % disable)
-        return self.rng_status().disable_aesni
+        return self.rng_status().disable_aes_ni
 
     def strong_rng(self, num_bytes=None):
         if (valid.Empty() | valid.Number(bounds=[0, None])).\