24c9a2a89e0bc0b3d83bc65ce270dc038a042c76
Fabian Deutsch <fabiand@fedoraproject.org>
2015-06-30 14:12:41 +0200

    tests: Fix name collision for nose tests
    Change-Id: Ifbcb535b199608d5aa53a28c9435811a218e36c3
    Signed-off-by: Fabian Deutsch <fabiand@fedoraproject.org>

--- src/ovirt/node/setup/__init__.py
+++ src/ovirt/node/setup/__init__.py
@@ -4,3 +4,12 @@ This package contains all plugins and the __main__ for the setup application.
 
 Each plugin can create it's own directory (a so called plugin group).
 """
+
+
+def __call__(self=None, *a, **k):
+    """FIXME this is a workaround to get nosetests working
+    nosetests sees this module as the setup() funciton for unit tests
+    This method is defined to provide a no-op
+    See also: https://code.google.com/p/python-nose/issues/detail?id=326
+    """
+    pass
