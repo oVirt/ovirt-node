4ac0578b435a0b9ac4a111824548f5df1f30dd8e
Fabian Deutsch <fabiand@fedoraproject.org>
2016-02-03 12:12:43 +0100

    sat: Fix logging message
    Previously the system name was not expanded in the logging message.
    
    Change-Id: I7c6fa16373a495ebd307393ded6473ed01da0da4
    Bug-Url: https://bugzilla.redhat.com/show_bug.cgi?id=1290297
    Signed-off-by: Fabian Deutsch <fabiand@fedoraproject.org>

--- src/ovirt/node/setup/rhn/rhn_model.py
+++ src/ovirt/node/setup/rhn/rhn_model.py
@@ -484,7 +484,8 @@ class RHN(NodeConfigFileSection):
                                               cfg["url"]))
                         else:
                             self.logger.info("System %s successfully "
-                                             "registered to SAM")
+                                             "registered to SAM" %
+                                             cfg["profile"])
 
                     # This isn't strictly necessary
                     if RHN().retrieve()["activationkey"]:
